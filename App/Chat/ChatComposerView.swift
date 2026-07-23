159040bd9a0009f226ce4106ed09ae7dcca492d4
e.palmisano <e.palmisano@reply.it>
2026-07-23 13:18:09 +0200

    feat: composer-anchored permission approvals and banner stack

--- App/Chat/ChatComposerView.swift
+++ App/Chat/ChatComposerView.swift
@@ -38,6 +38,8 @@ struct ChatComposerView: View {
                 mentionPopup(query: query)
             }
             queuedList
+            ComposerApprovalPanel(permissions: controller.composerPermissions,
+                                  controller: controller)
             card
         }
         .padding(.horizontal, 12)
