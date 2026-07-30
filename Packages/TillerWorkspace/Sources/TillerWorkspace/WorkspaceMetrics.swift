b6aacae8e2050d089cd0665ba87d4e7cd18f6675
e.palmisano <e.palmisano@reply.it>
2026-07-30 09:44:58 +0200

    feat: render the workspace tree with native split controllers
    refs CO-1, CO-4

---
+++ Packages/TillerWorkspace/Sources/TillerWorkspace/WorkspaceMetrics.swift
@@ -0,0 +1,8 @@
+import AppKit
+
+public enum WorkspaceMetrics {
+    public static let preferredGroupSize = CGSize(width: 240, height: 160)
+    public static let dividerThickness: CGFloat = 6
+    public static let edgeBandFraction: CGFloat = 0.22
+    public static let dragThreshold: CGFloat = 4
+}
